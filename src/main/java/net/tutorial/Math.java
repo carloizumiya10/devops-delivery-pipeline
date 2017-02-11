261cab20414130e31175df52687d27838f610d19
carloizumiya10 <carlo_izumiya@dlsu.edu.ph>
2017-02-11 10:22:21 +0800

    Update Math.java

--- src/main/java/net/tutorial/Math.java
+++ src/main/java/net/tutorial/Math.java
@@ -12,15 +12,17 @@ public class Math{
     }
   }
 
-  public int add(int a, int b){
-    return a+b;
+   public int add(int a, int b){
+    return a-b;
   }
   
   public int sub(int a, int b){
     return a-b;
   }  
 
-  public int multiply(int a, int b){
+ public int multiply(int a, int b){
+    delay();
     return a*b;
   }
+  
 }
